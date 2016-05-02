7af88af852f2aa49a0ea8ef90f7a7d2cb6eadf3c
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-05-02 22:58:37 +1200

    Start work on persistance code

---
+++ astatsscraper/persistence.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+class Persistor:
+    def __init__(self):
+        self.connection = sqlite3.connect('astatsscraper.db')
+        self.cursor = self.connection.cursor()
+
+    def init_database(self):
+        self.cursor.execute('''CREATE TABLE steam_apps(
+                                 app_id INTEGER,
+                                 name VARCHAR(255),
+                                 time_to_100 FLOAT,
+                                 total_points FLOAT
+                               )''')
