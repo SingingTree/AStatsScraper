dc8e2c91377a0c67f9dd9f2f2f64781ecf342333
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-07-18 20:42:17 +1200

    Tinker with GUI

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Requires Scrapy
   - Requires [Visual C++ for Python](http://aka.ms/vcpython27) on Windows
   - Requires [Pywin32 on](https://sourceforge.net/projects/pywin32/files/) Windows
 Requires [Requests](http://docs.python-requests.org/en/master/user/install/#pip-install-requests)
+Requires [Kivy](https://kivy.org/docs/installation/installation.html) for GUI
+    - Note that depending on your platform kivy has external deps.
--- astatsscraper/gui/gui.py
+++ astatsscraper/gui/gui.py
@@ -1,14 +1,33 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 
 
-class AStatsScraperGui(Widget):
-    pass
+class AStatsGameTable(GridLayout):
+    def __init__(self, **kwargs):
+        super(AStatsGameTable, self).__init__(**kwargs)
+        self.cols = 9
+        self.col_headers = [
+            Label(text="App Id"),
+            Label(text="Title"),
+            Label(text="Time to 100%"),
+            Label(text="Total Points"),
+            Label(text="Points Per Time"),
+            Label(text="# Players"),
+            Label(text="# Player to 100%"),
+            Label(text="% of Players to 100%"),
+            Label(text="Last Updated"),
+        ]
+
+        for label in self.col_headers:
+            label.size = label.texture_size
+            self.add_widget(label)
 
 
 class AStatsScraperApp(App):
     def build(self):
-        return AStatsScraperGui()
+        return AStatsGameTable()
 
 
 if __name__ == '__main__':
