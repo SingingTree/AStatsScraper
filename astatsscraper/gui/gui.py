08c500d863676822fa4b5847c1b9d0bd89fc3a5f
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-07-23 21:02:35 +1200

    Rework gui using lists so as to be performant

--- astatsscraper/gui/gui.py
+++ astatsscraper/gui/gui.py
@@ -1,45 +1,44 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.adapters.dictadapter import DictAdapter
+from kivy.adapters.listadapter import ListAdapter
+# ListViews are deprecated, but their replacement doesn't seem mature or in the kivy package yet
+from kivy.uix.listview import ListView, ListItemLabel, CompositeListItem
 from kivy.uix.gridlayout import GridLayout
 import astatsscraper.persistence
 
+def app_info_args_converter(row_index, app_info):
+    return {
+     'text': 'butts?',
+     'size_hint_y': None,
+     'height': 25,
+     'cls_dicts': [{'cls': ListItemLabel,
+                    'kwargs': {'text': unicode(app_info[0])}
+                    },
+                   {'cls': ListItemLabel,
+                    'kwargs': {'text': unicode(app_info[1])}
+                    },
+                   {'cls': ListItemLabel,
+                    'kwargs': {'text': unicode(app_info[2])}
+                    }]
+    }
 
 class AStatsGameTable(GridLayout):
     def __init__(self, **kwargs):
+        kwargs['cols'] = 1
         super(AStatsGameTable, self).__init__(**kwargs)
-        self.cols = 9
-        self.col_headers = [
-            Label(text='App Id'),
-            Label(text='Title'),
-            Label(text='Time to 100%'),
-            Label(text='Total Points'),
-            Label(text='Points Per Time'),
-            Label(text='# Players'),
-            Label(text='# Player to 100%'),
-            Label(text='% of Players to 100%'),
-            Label(text='Last Updated'),
-        ]
-
-        for label in self.col_headers:
-            label.size = label.texture_size
-            self.add_widget(label)
-
         with astatsscraper.persistence.Persistor() as persistor:
-            apps_info = persistor.get_all_apps_info()
+            items = persistor.get_all_apps_info()
 
-        for i, app_info in enumerate(apps_info):
-            row = []
-            try:
-                for item in app_info:
-                    row.append(Label(text=unicode(item)))
-            except UnicodeEncodeError:
-                    continue
-            # If no error creating row, stick it in the table
-            for label in row:
-                self.add_widget(label)
-            if i > 5:
-                break
+        dict_adapter = ListAdapter(data=items,
+                                   args_converter=app_info_args_converter,
+                                   selection_mode='single',
+                                   allow_empty_selection=False,
+                                   cls=CompositeListItem)
 
+        # Use the adapter in our ListView:
+        list_view = ListView(adapter=dict_adapter)
+
+        self.add_widget(list_view)
 
 
 
