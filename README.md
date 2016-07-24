351216aed8370c0b7f403c037a33412e8235fd9c
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-07-24 20:22:20 +1200

    Start reworking gui with PySide

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ Requires Scrapy
   - Requires [Visual C++ for Python](http://aka.ms/vcpython27) on Windows
   - Requires [Pywin32 on](https://sourceforge.net/projects/pywin32/files/) Windows
 Requires [Requests](http://docs.python-requests.org/en/master/user/install/#pip-install-requests)
-Requires [Kivy](https://kivy.org/docs/installation/installation.html) for GUI
-    - Note that depending on your platform kivy has external deps.
+Requires [PySide](https://pypi.python.org/pypi/PySide/1.2.4) for GUI
--- astatsscraper/gui/gui.py
+++ astatsscraper/gui/gui.py
@@ -1,52 +1,12 @@
-from kivy.app import App
-from kivy.adapters.dictadapter import DictAdapter
-from kivy.adapters.listadapter import ListAdapter
-# ListViews are deprecated, but their replacement doesn't seem mature or in the kivy package yet
-from kivy.uix.listview import ListView, ListItemLabel, CompositeListItem
-from kivy.uix.gridlayout import GridLayout
-import astatsscraper.persistence
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
 
-def app_info_args_converter(row_index, app_info):
-    return {
-     'text': 'butts?',
-     'size_hint_y': None,
-     'height': 25,
-     'cls_dicts': [{'cls': ListItemLabel,
-                    'kwargs': {'text': unicode(app_info[0])}
-                    },
-                   {'cls': ListItemLabel,
-                    'kwargs': {'text': unicode(app_info[1])}
-                    },
-                   {'cls': ListItemLabel,
-                    'kwargs': {'text': unicode(app_info[2])}
-                    }]
-    }
-
-class AStatsGameTable(GridLayout):
-    def __init__(self, **kwargs):
-        kwargs['cols'] = 1
-        super(AStatsGameTable, self).__init__(**kwargs)
-        with astatsscraper.persistence.Persistor() as persistor:
-            items = persistor.get_all_apps_info()
-
-        dict_adapter = ListAdapter(data=items,
-                                   args_converter=app_info_args_converter,
-                                   selection_mode='single',
-                                   allow_empty_selection=False,
-                                   cls=CompositeListItem)
-
-        # Use the adapter in our ListView:
-        list_view = ListView(adapter=dict_adapter)
-
-        self.add_widget(list_view)
-
-
-
-
-class AStatsScraperApp(App):
-    def build(self):
-        return AStatsGameTable()
-
-
-if __name__ == '__main__':
-    AStatsScraperApp().run()
+# Create a Qt application
+app = QApplication(sys.argv)
+# Create a Label and show it
+label = QLabel("Test")
+label.show()
+# Enter Qt application main loop
+app.exec_()
+sys.exit()
