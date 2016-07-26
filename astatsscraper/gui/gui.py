c46691a3e70c61b43c3a0ef940b4e0c32b9ff528
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-07-26 21:15:00 +1200

    Connect to DB with gui, working on table display

--- astatsscraper/gui/gui.py
+++ astatsscraper/gui/gui.py
@@ -1,12 +1,22 @@
 import sys
-from PySide.QtGui import QApplication, QMainWindow, QTableView
+from PySide.QtGui import QApplication, QMainWindow, QTableView, QAbstractItemView
+from PySide.QtSql import QSqlDatabase, QSqlTableModel
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        database = QSqlDatabase.addDatabase('QSQLITE')
+        database.setDatabaseName('astatsscraper.db') # Better lookup logic needed
+        if not database.open():
+            print('Error opening database!')
+        model = QSqlTableModel(db=database)
+        model.setTable('steam_apps')
         table = QTableView()
+        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        table.setModel(model)
         self.setCentralWidget(table)
+        table.show()
 
 app = QApplication(sys.argv)
 app.setApplicationName("AStatsScraperGui")
