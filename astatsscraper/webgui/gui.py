64f8a36d52f5a2981648dc029db79b84749c81be
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-07-29 19:06:11 +1200

    Improve upon tables in web GUI

--- astatsscraper/webgui/gui.py
+++ astatsscraper/webgui/gui.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+import json
 from astatsscraper.persistence import Persistor
 app =  Flask(__name__)
 
 @app.route('/')
 def hello_world():
     with Persistor() as persistor:
-        return render_template('GamesByPointsPerTime.html', apps_info=persistor.get_all_apps_info(order_by="points_per_time"))
+        return render_template('GamesByPointsPerTime.html', apps_info_json=json.dumps(persistor.get_all_apps_info(order_by="points_per_time")))
