514bdf9f46bd844aa98374bb81580c712c92da1f
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-08-18 21:49:49 +1200

    WIP: update persistance and db schema

--- astatsscraper/persistence.py
+++ astatsscraper/persistence.py
@@ -4,9 +4,13 @@ import datetime
 
 
 class Persistor:
+    def __init__(self, db_name='astatsscraper.db'):
+        self.db_name = db_name
+
     def __enter__(self):
-        self.connection = sqlite3.connect('astatsscraper.db')
+        self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
+        self.ensure_tables()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -14,7 +18,12 @@ class Persistor:
         self.connection.close()
 
     def ensure_tables(self):
-        print("ENSURING TABLES")
+        # Make sure tables are present
+        self.cursor.execute('''SELECT name FROM sqlite_master WHERE type = "table";''')
+        tables = [table for (table,) in self.cursor.fetchall()]
+        if 'users' in tables and 'owned_apps' in tables and 'steam_apps' in tables:
+            return
+
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS steam_apps(
                                  app_id INTEGER,
                                  title VARCHAR(255),
@@ -24,7 +33,10 @@ class Persistor:
                                  num_players INTEGER,
                                  num_players_to_100 INTEGER,
                                  percentage_of_players_to_100 FLOAT,
-                                 last_updated DATE,
+                                 astats_last_updated DATE,
+                                 recent_steam_rating INTEGER,
+                                 general_steam_rating INTEGER,
+                                 steampowered_last_updated DATE,
                                  PRIMARY KEY (app_id)
                                );''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS users(
@@ -43,38 +55,43 @@ class Persistor:
                                  FOREIGN KEY (app_id) REFERENCES steam_apps(app_id)
                                );''')
 
-    def store_app(self, app_item):
-        if app_item.get('total_points') == 0:
+    def store_astats_app(self, atats_app_item):
+        if atats_app_item.get('total_points') == 0:
             points_per_time = 0
-        elif app_item.get('time_to_100') == 0:
+        elif atats_app_item.get('time_to_100') == 0:
             points_per_time = None
         else:
-            points_per_time = app_item.get('total_points') / app_item.get('time_to_100')
-        if app_item.get('num_players') == 0:
+            points_per_time = atats_app_item.get('total_points') / atats_app_item.get('time_to_100')
+        if atats_app_item.get('num_players') == 0:
             percentage_to_hundo = 0
         else:
-            percentage_to_hundo = app_item.get('num_players_to_100') / app_item.get('num_players')
+            percentage_to_hundo = atats_app_item.get('num_players_to_100') / atats_app_item.get('num_players')
         self.cursor.execute('''INSERT OR REPLACE INTO steam_apps (app_id, title, time_to_100, total_points,
                                points_per_time, num_players, num_players_to_100, percentage_of_players_to_100,
-                               last_updated)
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);''',
+                               astats_last_updated, recent_steam_rating, general_steam_rating,
+                               steampowered_last_updated)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''',
                             (
-                                app_item.get('id'),
-                                app_item.get('title'),
-                                app_item.get('time_to_100'),
-                                app_item.get('total_points'),
+                                atats_app_item.get('id'),
+                                atats_app_item.get('title'),
+                                atats_app_item.get('time_to_100'),
+                                atats_app_item.get('total_points'),
                                 points_per_time,
-                                app_item.get('num_players'),
-                                app_item.get('num_players_to_100'),
+                                atats_app_item.get('num_players'),
+                                atats_app_item.get('num_players_to_100'),
                                 percentage_to_hundo,
                                 datetime.datetime.now(),
+                                None,
+                                None,
+                                None
                             ))
         self.connection.commit()
 
     def store_ownership(self, owned_app_item):
         self.cursor.execute('''INSERT OR IGNORE INTO steam_apps (app_id, title, time_to_100, total_points,
-                               points_per_time, num_players, num_players_to_100, last_updated)
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?);''',
+                               points_per_time, num_players, num_players_to_100, astats_last_updated,
+                               recent_steam_rating, general_steam_rating, steampowered_last_updated)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''',
                             (
                                 owned_app_item.get('app_id'),
                                 None,
@@ -84,6 +101,9 @@ class Persistor:
                                 None,
                                 None,
                                 datetime.datetime.now(),
+                                None,
+                                None,
+                                None,
                             ))
         self.cursor.execute('''INSERT OR REPLACE INTO users (steam_id, last_updated)
                                VALUES (?, ?);''',
@@ -106,8 +126,9 @@ class Persistor:
     def store_app_id(self, app_item):
         self.cursor.execute('''INSERT OR REPLACE INTO steam_apps (app_id, title, time_to_100, total_points,
                                points_per_time, num_players, num_players_to_100, percentage_of_players_to_100,
-                               last_updated)
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);''',
+                               astats_last_updated, recent_steam_rating, general_steam_rating,
+                               steampowered_last_updated)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''',
                             (
                                 app_item.get('id'),
                                 None,
@@ -117,7 +138,10 @@ class Persistor:
                                 None,
                                 None,
                                 None,
-                                datetime.datetime.now(),
+                                None,
+                                None,
+                                None,
+                                None,
                             ))
         self.connection.commit()
 
@@ -153,8 +177,10 @@ class Persistor:
 
     def get_owned_app_info(self, owner_id, order_by=None, asc_desc=None):
         query_string = 'SELECT steam_apps.app_id, title, time_to_100, total_points, points_per_time, num_players, ' \
-                       'num_players_to_100, percentage_of_players_to_100, steam_apps.last_updated FROM steam_apps ' \
-                       'INNER JOIN owned_apps ON steam_apps.app_id = owned_apps.app_id WHERE owned_apps.steam_id = ?'
+                       'num_players_to_100, percentage_of_players_to_100, steam_apps.astats_last_updated,' \
+                       'recent_steam_rating, general_steam_rating, steampowered_last_updated' \
+                       'FROM steam_apps INNER JOIN owned_apps ON steam_apps.app_id = owned_apps.app_id WHERE ' \
+                       'owned_apps.steam_id = ?'
         query_params = (owner_id,)
         if order_by:
             query_string += ' ORDER BY ?'
@@ -170,7 +196,8 @@ class Persistor:
 
     def get_all_apps_info(self, order_by=None, asc_desc=None):
         query_string = 'SELECT app_id, title, time_to_100, total_points, points_per_time, num_players, ' \
-                       'num_players_to_100, percentage_of_players_to_100, last_updated FROM steam_apps'
+                       'num_players_to_100, percentage_of_players_to_100, astats_last_updated, recent_steam_rating,' \
+                       'general_steam_rating, steampowered_last_updated FROM steam_apps'
         query_params = ()
         if order_by:
             query_string += ' ORDER BY ?'
--- astatsscraper/pipelines.py
+++ astatsscraper/pipelines.py
@@ -24,7 +24,7 @@ class SteamAppPipeline(object):
     def process_item(self, item, spider):
         if self.__class__ in spider.pipeline:
             with persistence.Persistor() as persistor:
-                persistor.store_app(item)
+                persistor.store_astats_app(item)
             return item
         else:
             return item
